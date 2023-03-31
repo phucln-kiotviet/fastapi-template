ec2334ad4a570d76996485ad6ed69ea57a647af3
phucln <phuc.ln@kiotviet.com>
2023-03-31 16:45:20 +0700

    add tags for route to update docs, check /docs to view

--- src/articles/router.py
+++ src/articles/router.py
@@ -9,32 +9,32 @@ from uuid import UUID
 router = APIRouter()
 
 
-@router.get("/", response_model=Page[schemas.Articles])
+@router.get("/", response_model=Page[schemas.Articles], tags=['list articles'])
 def get_list_articles(db: Session = Depends(get_db)):
     return paginate(service.get_list_articles(db))
 
 
-@router.get("/{articles_id}", response_model=schemas.Articles)
+@router.get("/{articles_id}", response_model=schemas.Articles, tags=['CRUD articles'])
 def get_articles(articles_id: UUID, db: Session = Depends(get_db)):
     return service.get_articles(db=db, articles_id=articles_id)
 
 
-@router.post("/", response_model=schemas.Articles, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.Articles, status_code=status.HTTP_201_CREATED, tags=['CRUD articles'])
 def create_articles(
     articles: schemas.ArticlesCreate, db: Session = Depends(get_db)
 ):
     return service.create_articles(db=db, articles=articles)
 
 
-@router.delete("/{articles_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_articles(articles_id: UUID, db: Session = Depends(get_db)):
-    return service.delete_articles(db=db, articles_id=articles_id)
-
-
-@router.patch("/{articles_id}", response_model=schemas.ArticlesUpdate)
+@router.patch("/{articles_id}", response_model=schemas.ArticlesUpdate, tags=['CRUD articles'])
 def update_articles(
     articles_id: UUID,
     update_fields: schemas.ArticlesUpdate,
     db: Session = Depends(get_db)
 ):
     return service.update_articles(db, articles_id=articles_id, updated_fields=update_fields)
+
+
+@router.delete("/{articles_id}", status_code=status.HTTP_204_NO_CONTENT, tags=['CRUD articles'])
+def delete_articles(articles_id: UUID, db: Session = Depends(get_db)):
+    return service.delete_articles(db=db, articles_id=articles_id)
--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
-@app.get("/health")
+@app.get("/health", tags=['monitoring'])
 def health_check():
     return {"message": "ok"}
 
