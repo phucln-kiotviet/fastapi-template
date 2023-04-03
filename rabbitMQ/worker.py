2e64ab0b1895d9d20b75386b5f925d3162b38550
phucln <phuc.ln@kiotviet.com>
2023-04-03 10:36:26 +0700

    second tutorial two on rabbitMQ documents

--- rabbitMQ/worker.py
+++ rabbitMQ/worker.py
@@ -1,5 +1,5 @@
-import time
 import pika
+import time
 import sys
 import os
 
@@ -11,16 +11,19 @@ def main():
         pika.ConnectionParameters(RABBITMQ_URL))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(
+        queue='task_queue',
+        durable=True  # Make sure queue will survive a RabbitMQ node restart
+    )
 
     def callback(channel, method, properties, body):
         print("[x] Received %r" % body.decode())
         time.sleep(body.count(b'.'))
         print("[x] Done")
-        channel.basic_ack(delivery_tag = method.delivery_tag)
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_consume(queue='hello',
-                        #   auto_ack=True,
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue='task_queue',
                           on_message_callback=callback)
 
     print('[x] Waiting for message. To exit press CTRL+C: ')
